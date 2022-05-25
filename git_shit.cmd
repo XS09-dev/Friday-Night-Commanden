1df640070837acc0038528536438979102790c3b
XS_09-Dev <81271362+XS09-dev@users.noreply.github.com>
2022-05-25 12:00:23 -0400

    Delete git_shit.cmd

--- git_shit.cmd
+++
@@ -1,3 +0,0 @@
-haxelib git polymod https://github.com/larsiusprime/polymod.git
-haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
-haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
